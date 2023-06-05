76271043389994c26a2158032fc3a7c0396c49a0
Kirillos2811 <kirill.sesorov@gmail.com>
2023-06-06 00:23:44 +0300

    Lesson 11 task 1

--- task1.cpp
+++ task1.cpp
@@ -1,13 +1,23 @@
-#include <unistd.h>
+#include <fstream>
 #include <sstream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    if (fork()){
-        sleep(120);
-    }
-    else{
-        exit(0);
+    ifstream ifile("coordinates.txt", ios::in);
+    string str;
+    while (getline(ifile, str)){
+        stringstream ss(str);
+        float latitude, longtitude;
+        ss.ignore();
+        ss >> latitude;
+        ss.ignore();
+        ss.ignore();
+        ss >> longtitude;
+        if ((latitude >= 50 and latitude <= 80) and (longtitude >= 20 and longtitude <= 45)){
+            cout << str << endl;
+        }
     }
     return 0;
 }
