f6567c41afee50e15dc509fc5f537ec7f3bfa755
Kirillos2811 <kirill.sesorov@gmail.com>
2023-06-07 22:42:48 +0300

    Lesson 12 task 1

--- task1.cpp
+++ task1.cpp
@@ -1,23 +1,49 @@
-#include <fstream>
-#include <sstream>
 #include <iostream>
-#include <string>
+#include <numeric>
+#include <pthread.h>
 using namespace std;
 
+void* multiply(void* args){
+    float** args_array = (float **) args;
+    *args_array[2] = *args_array[0] * (*args_array[1]);
+    return 0;
+}
+
 int main(){
-    ifstream ifile("coordinates.txt", ios::in);
-    string str;
-    while (getline(ifile, str)){
-        stringstream ss(str);
-        float latitude, longtitude;
-        ss.ignore();
-        ss >> latitude;
-        ss.ignore();
-        ss.ignore();
-        ss >> longtitude;
-        if ((latitude >= 50 and latitude <= 80) and (longtitude >= 20 and longtitude <= 45)){
-            cout << str << endl;
+    int l = 3;
+    int m = 2;
+    int n = 3;
+
+    float matrix1[l][m] = {
+        {1, 2},
+        {3, 4},
+        {5, 6}
+    };
+    float matrix2[m][n] = {
+        {7, 8, 9},
+        {10, 11, 12}
+    };
+    float res_matrix[l][n];
+
+    for (int i = 0; i < l; i++){
+        for (int j = 0; j < n; j++){
+            float temp_array[m];
+            for (int r = 0; r < m; r++){
+                pthread_t thread;
+                float* args[3] = {&matrix1[i][r], &matrix2[r][j], &temp_array[r]};
+                pthread_create(&thread, NULL, multiply, (void *) args);
+                pthread_join(thread, NULL);
+            }
+            res_matrix[i][j] = accumulate(temp_array, temp_array + m, 0.0);
         }
+    }
+
+    printf("Resulting matrix\n");
+    for (int i = 0; i < l; i++){
+        for (int j = 0; j < n; j++){
+            printf("%f ", res_matrix[i][j]);
+        }
+        printf("\n");
     }
     return 0;
 }
